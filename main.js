32bed7f6946239f0cafb9153de4432bf5127cec4
enots0514 <aattitude@daum.net>
2020-10-09 02:44:02 +0900

    모둘 연결 설명 추가

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const myModule = require("./module.js");
+        /* 모듈js을 모듈 폴더에 넣고 index.js로 바꿈
+        const myModule = require("./module");
+        그러면 폴더명만 써서 사용함. 이 방법이 많이 사용됨
+        /*
 
 console.log(myModule);
 console.log(myModule.a);
