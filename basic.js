e6b60da273b2c3113da59a719201117fd72635e2
enots0514 <aattitude@daum.net>
2020-10-09 01:06:32 +0900

    노드js 서버만들기

---
+++ basic.js
@@ -0,0 +1,27 @@
+// 모듈을 추출한다.
+const http = require('http');
+// 웹 서버를 만들고 실행한다.
+http.createServer(function(request, response){
+    response.writeHead(200, {'Content-Type':'text-html'});
+    response.end('<h1>Hello World</h1>');
+}).listen(3000, function(){
+    console.log('Server running at http://127.0.0.1:3000'); 
+});
+
+
+/* 공식문서 자료
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+*/
