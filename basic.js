90e97c6071b6291751f64f50f4762e02745ec99b
enots0514 <aattitude@daum.net>
2020-10-09 01:20:06 +0900

    nodejs서버올리기 ecma6버전

--- basic.js
+++ basic.js
@@ -1,3 +1,5 @@
+
+/* 1_nodejs 시작 기본
 // 모듈을 추출한다.
 const http = require('http');
 // 웹 서버를 만들고 실행한다.
@@ -5,6 +7,17 @@ http.createServer(function(request, response){
     response.writeHead(200, {'Content-Type':'text-html'});
     response.end('<h1>Hello World</h1>');
 }).listen(3000, function(){
+    console.log('Server running at http://127.0.0.1:3000'); 
+});
+*/
+
+//ecma6로 변환
+const http = require('http');
+// 웹 서버를 만들고 실행한다.
+http.createServer((request, response) => {
+    response.writeHead(200, {'Content-Type':'text-html'});
+    response.end('<h1>Enots0514 Home Coding World</h1>');
+}).listen(3000, () => {
     console.log('Server running at http://127.0.0.1:3000'); 
 });
 
